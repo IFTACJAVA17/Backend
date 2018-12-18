ff96b427ff39f24aa9b64364c02b78e67f587b83
Holmkvist <32063102+Holmkvist@users.noreply.github.com>
2018-12-18 10:51:22 +0100

    Add files via upload

---
+++ src/main/java/se/iftac/iftacgames/IftacgamesApplication.java
@@ -0,0 +1,14 @@
+package se.iftac.iftacgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IftacgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IftacgamesApplication.class, args);
+	}
+
+}
+
