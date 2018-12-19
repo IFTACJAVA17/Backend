654849ef1615494f61e39bee8e77d0d1b3a05479
Patrik Holmkvist <patrik.holmkvist@outlook.com>
2018-12-19 14:41:04 +0100

    add connection to database, hello world example on user controller

--- src/main/java/se/iftac/iftacgames/rest/UserController.java
+++ src/main/java/se/iftac/iftacgames/rest/UserController.java
@@ -1,9 +1,15 @@
 package se.iftac.iftacgames.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("Hello")
+    public String getHello() {
+        return "Hello World";
+    }
 }
